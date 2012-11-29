7c0d3d104d97ed5d6ae4fbdf28dd205a942f57b7
302661701 <302661701@qq.com>
2012-11-29 15:51:28 +0000

    ObjLoader的bug修改 之前写的太大意。。

--- ObjLoader/ObjLoader/D3D9Mesh.cpp
+++ ObjLoader/ObjLoader/D3D9Mesh.cpp
@@ -32,7 +32,7 @@ bool D3D9Mesh::SetUp(Loader& loader)
 		Ptr<D3D9IndexBuffer> pIB = new D3D9IndexBuffer();
 
 
-		int vertexCount = content[0].mVertexBuffer.size()/8;
+		int vertexCount = content[iSubMesh].mVertexBuffer.size()/8;
 
 		if( FAILED( D3D9Device::GetInstance().GetD3DDevice9()->CreateVertexBuffer(vertexCount*sizeof( CUSTOMVERTEX ),
 			0, D3DFVF_CUSTOMVERTEX,
@@ -42,18 +42,18 @@ bool D3D9Mesh::SetUp(Loader& loader)
 		}
 		CUSTOMVERTEX* pVertices ;
 		pVB->GetD3D9VertexBufferPtr()->Lock(0, 0, ( void** )&pVertices, 0 );
-		memcpy(pVertices,&content[0].mVertexBuffer[0],vertexCount*sizeof( CUSTOMVERTEX ));
+		memcpy(pVertices,&content[iSubMesh].mVertexBuffer[0],vertexCount*sizeof( CUSTOMVERTEX ));
 		pVB->GetD3D9VertexBufferPtr()->Unlock();
 
 
-		if( FAILED( D3D9Device::GetInstance().GetD3DDevice9()->CreateIndexBuffer(content[0].mVertexIndexBuffer.size()*sizeof( int ),0,D3DFMT_INDEX32,D3DPOOL_DEFAULT,pIB->GetD3D9IndexBufferPP(),NULL ) ) )
+		if( FAILED( D3D9Device::GetInstance().GetD3DDevice9()->CreateIndexBuffer(content[iSubMesh].mVertexIndexBuffer.size()*sizeof( int ),0,D3DFMT_INDEX32,D3DPOOL_DEFAULT,pIB->GetD3D9IndexBufferPP(),NULL ) ) )
 		{
 			return false;
 		}
 
 		int* pIndex ;
 		pIB->GetD3D9IndexBufferPtr()->Lock(0, 0,( void** )&pIndex, 0 );
-		memcpy(pIndex,&content[0].mVertexIndexBuffer[0],content[0].mVertexIndexBuffer.size()*sizeof( int ));
+		memcpy(pIndex,&content[iSubMesh].mVertexIndexBuffer[0],content[iSubMesh].mVertexIndexBuffer.size()*sizeof( int ));
 		pIB->GetD3D9IndexBufferPtr()->Unlock();
 
 		m_pVBArray.push_back(pVB);
