855169ff9b2a4f60538decae498000967dcf6f03
302661701 <302661701@qq.com>
2012-12-24 14:49:36 +0000

    标题：抄袭加载dds代码，稍后做代码整理

--- ObjLoader/ObjLoader/D3D9Texture.cpp
+++ ObjLoader/ObjLoader/D3D9Texture.cpp
@@ -4,8 +4,6 @@
 #include "TexLoader.h"
 #include "UtilTex.h"
 
-#include <d3dx9.h>
-
 D3D9Texture::D3D9Texture()
 {
 
@@ -16,42 +14,95 @@ D3D9Texture::~D3D9Texture()
 }
 bool D3D9Texture::SetUp(Loader& loader)
 {
-	HRESULT hr = D3DXCreateTextureFromFile(D3D9Device::GetInstance().GetD3DDevice9(),"cup.jpg",&m_pD3D9Texture);
-	if (hr!=S_OK)
-	{
-		return false;
-	}
-	return true;
 
-// 	TexLoader& texLoader = (TexLoader&)loader;
-// 	IDirect3DDevice9* device = D3D9Device::GetInstance().GetD3DDevice9();
-// 	DDSTexDataInfo* textureInfo = texLoader.GetTexDataInfo();
-// 	D3DFORMAT fmt = GetD3D9Format( textureInfo->mDDS_HEADER->ddspf );
-// 	HRESULT hr = device->CreateTexture( textureInfo->mDDS_HEADER->dwWidth,
-// 	 textureInfo->mDDS_HEADER->dwHeight,
-// 	 textureInfo->mDDS_HEADER->dwMipMapCount,
-// 	 D3DUSAGE_DYNAMIC, // usage
-// 	 fmt,
-// 	 D3DPOOL_MANAGED,
-// 	 &m_pD3D9Texture,
-// 	 NULL );
-// 	if(hr == S_OK)
-// 	{
-// 
-// 		D3DLOCKED_RECT d3dlr;
-// 		hr = m_pD3D9Texture->LockRect(0,&d3dlr,NULL,0 );
-// 		if ( hr!=S_OK)
-// 		{
-// 			return false;
-// 		}
-// 		memcpy(d3dlr.pBits,texLoader.GetTexDataInfo()->mTexData,texLoader.GetTexDataInfo()->mTexDataSize);
-// 		m_pD3D9Texture->UnlockRect(0);
-// 		return true;
-// 	}
-// 	else
-// 	{
-// 		return false;
-// 	}
+ 	TexLoader& texLoader = (TexLoader&)loader;
+ 	IDirect3DDevice9* device = D3D9Device::GetInstance().GetD3DDevice9();
+ 	DDSTexDataInfo* textureInfo = texLoader.GetTexDataInfo();
+	DDS_HEADER* ddsHeader = textureInfo->mDDS_HEADER;
+ 	D3DFORMAT fmt = GetD3D9Format( textureInfo->mDDS_HEADER->ddspf );
+
+	HRESULT hr = S_OK;
+    D3DLOCKED_RECT LockedRect = {0};
+
+	UINT iWidth =ddsHeader->dwWidth;
+    UINT iHeight = ddsHeader->dwHeight;
+    UINT iMipCount = ddsHeader->dwMipMapCount;
+    if( 0 == iMipCount )
+        iMipCount = 1;
+
+    if (ddsHeader->dwCubemapFlags != 0
+        || (ddsHeader->dwHeaderFlags & DDS_HEADER_FLAGS_VOLUME) )
+    {
+        // For now only support 2D textures, not cubemaps or volumes
+        return E_FAIL;
+    }
+
+
+    // Create the texture
+    LPDIRECT3DTEXTURE9 pTexture;
+    LPDIRECT3DTEXTURE9 pStagingTexture;
+    hr = device->CreateTexture( iWidth,
+                              iHeight,
+                              iMipCount,
+                              0, // usage
+                              fmt,
+                              D3DPOOL_DEFAULT,
+                              &pTexture,
+                              NULL );
+    if( FAILED( hr ) )
+        return hr;
+
+    hr = device->CreateTexture( iWidth,
+                              iHeight,
+                              iMipCount,
+                              0, // usage
+                              fmt,
+                              D3DPOOL_SYSTEMMEM,
+                              &pStagingTexture,
+                              NULL );
+    if( FAILED( hr ) )
+    {
+        return hr;
+    }
+
+    // Lock, fill, unlock
+    UINT RowBytes, NumRows;
+	BYTE* pSrcBits = textureInfo->mTexData;
+
+    for( UINT i = 0; i < iMipCount; i++ )
+    {
+        GetSurfaceInfo( iWidth, iHeight, fmt, NULL, &RowBytes, &NumRows );
+
+        if( SUCCEEDED( pStagingTexture->LockRect( i, &LockedRect, NULL, 0 ) ) )
+        {
+            BYTE* pDestBits = ( BYTE* )LockedRect.pBits;
+
+            // Copy stride line by line
+            for( UINT h = 0; h < NumRows; h++ )
+            {
+                CopyMemory( pDestBits, pSrcBits, RowBytes );
+                pDestBits += LockedRect.Pitch;
+                pSrcBits += RowBytes;
+            }
+
+            pStagingTexture->UnlockRect( i );
+        }
+
+        iWidth = iWidth >> 1;
+        iHeight = iHeight >> 1;
+        if( iWidth == 0 )
+            iWidth = 1;
+        if( iHeight == 0 )
+            iHeight = 1;
+    }
+
+    hr = device->UpdateTexture( pStagingTexture, pTexture );
+    if( FAILED( hr ) )
+        return hr;
+
+    // Set the result
+    m_pD3D9Texture = pTexture;
+    return hr;
 
 
 }
@@ -60,3 +111,4 @@ IDirect3DTexture9* D3D9Texture::GetD3D9TexturePtr()
 {
     return m_pD3D9Texture;
 }
+
