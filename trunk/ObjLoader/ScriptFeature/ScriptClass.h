a09588306142eb5333374e752d6f10c73ff2f6af
302661701@qq.com <302661701@qq.com@efc225e3-2c50-549e-2d08-bf7e5383659c>
2012-12-29 17:04:43 +0000

    用实现函数互相调用初步验证了脚本基础没有问题

---
+++ trunk/ObjLoader/ScriptFeature/ScriptClass.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "PreCompiledHeaders.h"
+#include "MonoApi.h"
+#include "MonoType.h"
+#include <map>
+#include <string>
+
+
+
+namespace App
+{
+	
+	class ScriptClass
+	{
+	public:
+		ScriptClass();
+		virtual ~ScriptClass();
+		
+		void Init( MonoImage* pImage,const std::string& namespaceName ,const std::string& className);
+
+		MonoMethod* GetMethod( const std::string& sig);
+
+		MonoClass* GetMonoClass()
+		{
+			return m_pClass;
+		}
+	protected:
+		MonoClass* m_pClass;
+		std::map<std::string,MonoMethod*>  m_mMethodCache;
+	};
+}
